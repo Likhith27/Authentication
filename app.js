aac719a26ec0fb8f97bf4e728ebc07945389171c
Likhith Kumar Lakkireddy <78678281+Likhith27@users.noreply.github.com>
2022-07-15 20:57:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+//jshint esversion:6
+
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const e = require('express');
+const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+console.log(process.env.API_KEY);
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+//////////////////////////////////////////////////////////
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+//encryption process through mongoose-encryption
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ['password']});
+
+const User = new mongoose.model("User",userSchema); 
+
+
+
+app.get('/',function(req,res){
+
+    res.render("home");
+    
+})
+
+app.get('/login',function(req,res){
+
+    res.render("login");
+    
+})
+
+app.get('/register',function(req,res){
+
+    res.render("register");
+    
+})
+
+app.post('/register',function(req,res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    }
+
+    );
+    newUser.save(function(err){
+        if(!err){
+           res.render("secrets");
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+app.post('/login', function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    User.findOne({email: username},function(err,user){
+        if(err){
+            console.log(err);
+        }else{
+            if(user)
+            {
+                if(user.password === password){
+                    res.render("secrets")
+                }
+            }
+        }
+    });
+    
+});
+
+
+
+
+
+/////////////////////////////////////////////////////////
+app.listen(3000, function(){
+    console.log('Server running at http://localhost:3000');
+})
+
